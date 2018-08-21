2893e027f42446b0912ae0f41873d126df9c1633
Sebastian K <lasergottmc@gmail.com>
2018-08-21 19:19:03 +0200

    1.0.2 - change variables

--- src/main/java/cloud/timo/cloudtablist/listeners/PlayerDisconnectListener.java
+++ src/main/java/cloud/timo/cloudtablist/listeners/PlayerDisconnectListener.java
@@ -18,7 +18,7 @@ public class PlayerDisconnectListener implements Listener {
         TimoCloudAPI.getUniversalAPI().getProxyGroups().forEach(proxyGroupObject -> proxyGroupObject.getProxies().forEach(proxyObject -> {
             proxyObject.getOnlinePlayers().forEach(playerObject -> {
                 if (!playerObject.getUuid().equals(event.getPlayer().getUuid()))
-                    CloudTablist.getInstance().getHelper().setTablist(event.getPlayer());
+                    CloudTablist.getInstance().getHelper().setTablist(playerObject);
             });
         }));
     }
